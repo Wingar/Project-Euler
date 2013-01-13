dd408c52acfe1ceb8c5e1234ebf205652dba7c42
Emily Ozols <wingar@team-metro.net>
2013-01-13 19:03:39 +1100

    Problem 5

--- Program.cs
+++ Program.cs
@@ -12,34 +12,16 @@ namespace Projecteuler
             Console.WriteLine("Press any key to start!");
             Console.ReadKey();
             Console.WriteLine("Mathemating...");
-            int large = 0;
-            for (int i = 0; i <= 999; i++) //Iterate i 999 times.
+            int i = 20;
+
+            while (i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0 ||i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 || i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 || i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 || i % 18 != 0 || i % 19 != 0 || i % 20 != 0)
             {
-                for (int l = 0; l <= 999; l++) // Iterate l 999 times.
-                    if (isPalindrome((l * i).ToString()) == true) // If the multiplication of l by i is a palindrome...
-                        if ((l * i) > large) // If the palindrome is larger than the previously large palindrome, replace it.
-                            large = (i * l);
+                i++;
             }
-            Console.WriteLine("Result: {0}", large); // Print Result!
+
+            Console.WriteLine("Result: {0}", i); // Print Result!
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
-        }
-        static bool isPalindrome(string n)
-        {
-            int half = n.Length / 2; // Get half of the length of the string
-            string half1 = n.Substring(0, half); // Get the first half of the string
-            string half2 = Reverse(n.Substring(half, half)); // Get the second half of the string and reverse it
-            if (half1 == half2) // If the two strings match, return as true.
-                return true;
-            else
-                return false;
-        }
-        public static string Reverse(string s)
-        {
-            // Reverse the given string. Pretty fucking simple you dolt, why are you reading this.
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
         }
     }
 }
