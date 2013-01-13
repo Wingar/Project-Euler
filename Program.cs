393d9695eb55e4b3201698e5b7a7a141f10eaa8a
Emily Ozols <wingar@team-metro.net>
2013-01-13 22:21:19 +1100

    Problem 10.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,59 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Projecteuler
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             Console.WriteLine("Press any key to start!");
             Console.ReadKey();
             Console.WriteLine("Mathemating...");
-            int sum = 1000;
-            int a;
-            for (a = 1; a <= sum / 3; a++)
+            long total = 0;
+            for (long i = 1; i <= 2000000; i++)
             {
-                int b;
-                for (b = a + 1; b <= sum / 2; b++)
-                {
-                    int c = sum - a - b;
-                    if (c > 0 && (a * a + b * b == c * c))
-                        Console.WriteLine("{0} + {1} + {2} = 1000", a, b, c);
-                }
+                if (isPrime(i))
+                    total += i;
             }
-
-            //Console.WriteLine("{0} + {1} + {2} = 1000", a, b, c); // Print Result!
+            Console.WriteLine("Result: {0}", total);
+            Console.WriteLine("Length: {0}", total.ToString().Length);
+            Clipboard.SetText(total.ToString());
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+        }
+        static bool isPrime(long n)
+        {
+            if (n == 1)
+                return false;
+            else if (n < 4)
+                return true;
+            else if (n % 2 == 0)
+                return false;
+            else if (n < 9)
+                return true;
+            else if (n % 3 == 0)
+                return false;
+            else
+            {
+                long r = (long)Math.Floor(Math.Sqrt(n));
+                long f = 5;
+                while (f <= r)
+                {
+                    if (n % f == 0)
+                        return false;
+                    if (n % (f + 2) == 0)
+                        return false;
+
+                    f = f + 6;
+                }
+                return true;
+            }
         }
     }
 }
