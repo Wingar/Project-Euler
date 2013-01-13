671ee4dd14cc34962c2ee5f1652d89f5d0c4a7fd
Emily Ozols <wingar@team-metro.net>
2013-01-13 19:01:51 +1100

    Problem 2.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projecteuler
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            long last = 0;
+            long result = 1;
+
+            for (int i = 0; last <= 4000000; i++)
+            {
+                last = recFib(i);
+                Console.WriteLine(last);
+                if (last % 2 == 0) result += last;
+            }
+            Console.WriteLine("Result: {0}",result - 1);
+            Console.ReadKey();
+        }
+        static long recFib(int n)
+        {
+            if (n < 2) return n;
+            return recFib(n - 1) + recFib(n - 2);
+        }
+    }
+}
