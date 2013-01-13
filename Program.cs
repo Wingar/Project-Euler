0c425c4e72122e0599896512c26c1b70f5433f6a
Emily Ozols <wingar@team-metro.net>
2013-01-14 00:12:20 +1100

    Problem 11.
    IT FUCKING WORKED. FUCK YEAH.

--- Program.cs
+++ Program.cs
@@ -3,57 +3,191 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows.Forms;
+using System.Reflection;
 
 namespace Projecteuler
 {
     class Program
     {
-        [STAThread]
         static void Main(string[] args)
         {
             Console.WriteLine("Press any key to start!");
             Console.ReadKey();
             Console.WriteLine("Mathemating...");
-            long total = 0;
-            for (long i = 1; i <= 2000000; i++)
+            /* Whale, whale, whale. What do we have here?
+                                      . .
+                                    '.-:-.`  
+                                    '  :  `
+                                 .-----:     
+                               .'       `.
+                         ,    /       (o) \   
+                         \`._/          ,__)
+                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+             */
+            string[] grid = new string[] {  "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08",
+                                            "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00",
+                                            "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65",
+                                            "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91",
+                                            "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80",
+                                            "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50",
+                                            "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70",
+                                            "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21",
+                                            "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72",
+                                            "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95",
+                                            "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92",
+                                            "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57",
+                                            "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58",
+                                            "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40",
+                                            "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66",
+                                            "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69",
+                                            "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36",
+                                            "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16",
+                                            "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54",
+                                            "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48" };
+
+            // Woah, what the fuck is going on?! I'm crying.
+            string[][] level = { grid[0].Split(' '),
+                               grid[1].Split(' '),
+                               grid[2].Split(' '),
+                               grid[3].Split(' '),
+                               grid[4].Split(' '),
+                               grid[5].Split(' '),
+                               grid[6].Split(' '),
+                               grid[7].Split(' '),
+                               grid[8].Split(' '),
+                               grid[9].Split(' '),
+                               grid[10].Split(' '),
+                               grid[11].Split(' '),
+                               grid[12].Split(' '),
+                               grid[13].Split(' '),
+                               grid[14].Split(' '),
+                               grid[15].Split(' '),
+                               grid[16].Split(' '),
+                               grid[17].Split(' '),
+                               grid[18].Split(' '),
+                               grid[19].Split(' ') };
+            int final = 0;
+
+            for (int x = 0; x <= 19; x++) // Move Vertically.
             {
-                if (isPrime(i))
-                    total += i;
+                for (int y = 0; y <= 19; y++) // Move Horizonally
+                {
+                    if (y < 16)// There has to be 4 blocks below for it to check.
+                    {
+                        int result = int.Parse(level[x][y])
+                            * int.Parse(level[x][y + 1])
+                            * int.Parse(level[x][y + 2])
+                            * int.Parse(level[x][y + 3]);
+                        if (result > final)
+                            final = result;
+                    }
+                    if (y > 4 ) // Checking up, if possible.
+                    {
+                        int result = int.Parse(level[x][y])
+                            * int.Parse(level[x][y - 1])
+                            * int.Parse(level[x][y - 2])
+                            * int.Parse(level[x][y - 3]);
+                        if (result > final)
+                            final = result;
+                    }
+                    if (x < 16) // 4 block grace. Checking Right.
+                    {
+                        int result = int.Parse(level[x][y])
+                            * int.Parse(level[x + 1][y])
+                            * int.Parse(level[x + 2][y])
+                            * int.Parse(level[x + 3][y]);
+                        if (result > final)
+                            final = result;
+                    }
+                    if (x > 4) // Checking left.
+                    {
+                        int result = int.Parse(level[x][y])
+                            * int.Parse(level[x - 1][y])
+                            * int.Parse(level[x - 2][y])
+                            * int.Parse(level[x - 3][y]);
+                        if (result > final)
+                            final = result;
+                    }
+                    if (y < 16)
+                        if (x < 16) // Making sure there's 4 diagonal blocks for it to check.
+                        {
+                            int result = int.Parse(level[x][y])
+                            * int.Parse(level[x + 1][y + 1])
+                            * int.Parse(level[x + 2][y + 2])
+                            * int.Parse(level[x + 3][y + 3]);
+                            if (result > final)
+                                final = result;
+                        }
+                    if (y > 4) // Reverse diagonal.
+                        if (x > 4)
+                        {
+                            int result = int.Parse(level[x][y])
+                            * int.Parse(level[x - 1][y - 1])
+                            * int.Parse(level[x - 2][y - 2])
+                            * int.Parse(level[x - 3][y - 3]);
+                            if (result > final)
+                                final = result;
+                        }
+                    if (y > 4) // More diagonals!
+                        if (x < 16) 
+                        {
+                            int result = int.Parse(level[x][y])
+                            * int.Parse(level[x + 1][y - 1])
+                            * int.Parse(level[x + 2][y - 2])
+                            * int.Parse(level[x + 3][y - 3]);
+                            if (result > final)
+                                final = result;
+                        }
+                    if (y < 16)
+                        if (x > 4)
+                        {
+                            int result = int.Parse(level[x][y])
+                            * int.Parse(level[x - 1][y + 1])
+                            * int.Parse(level[x - 2][y + 2])
+                            * int.Parse(level[x - 3][y + 3]);
+                            if (result > final)
+                                final = result;
+                        }
+                }
             }
-            Console.WriteLine("Result: {0}", total);
-            Console.WriteLine("Length: {0}", total.ToString().Length);
-            Clipboard.SetText(total.ToString());
+            Console.WriteLine("Final result is: {0}", final);
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
-        static bool isPrime(long n)
-        {
-            if (n == 1)
-                return false;
-            else if (n < 4)
-                return true;
-            else if (n % 2 == 0)
-                return false;
-            else if (n < 9)
-                return true;
-            else if (n % 3 == 0)
-                return false;
-            else
-            {
-                long r = (long)Math.Floor(Math.Sqrt(n));
-                long f = 5;
-                while (f <= r)
-                {
-                    if (n % f == 0)
-                        return false;
-                    if (n % (f + 2) == 0)
-                        return false;
-
-                    f = f + 6;
-                }
-                return true;
-            }
-        }
     }
 }
+// HOLY FUCK THIS ACTUALLY GAVE THE RIGHT ANSWER.
+// OH MY GOOOODDDDDDDDDDDDD
+/*
+                                 _
+                              ==(W{==========-      /===-
+                                ||  (.--.)         /===-_---~~~~~~~----__
+                                | \_,|**|,__      |===-~___            _,-'`
+                   -==\\        `\ ' `--'   ),    `//~\\   ~~~~`--._.-~
+               ______-==|        /`\_. .__/\ \    | |  \\          _-~`
+         __--~~~  ,-/-==\\      (   | .  |~~~~|   | |   `\       ,'
+      _-~       /'    |  \\     )__/==0==-\<>/   / /      \     /
+    .'        /       |   \\      /~\___/~~\/  /' /        \   /
+   /  ____  /         |    \`\.__/-~~   \  |_/'  /          \/'
+  /-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`
+                    \_|      /        _) | ;  ),   __--~~
+                      '~~--_/      _-~/- |/ \   '-~ \
+                     {\__--_/}    / \\_>-|)<__\      \
+                     /'   (_/  _-~  | |__>--<__|      |
+                    |   _/) )-~     | |__>--<__|      |
+                    / /~ ,_/       / /__>---<__/      |
+                   o-o _//        /-~_>---<__-~      /
+                   (^(~          /~_>---<__-      _-~
+                  ,/|           /__>--<__/     _-~
+               ,//('(          |__>--<__|     /                  .--_
+              ( ( '))          |__>--<__|    |                 /' _-_~\
+           `-)) )) (           |__>--<__|    |               /'  /   ~\`\
+          ,/,'//( (             \__>--<__\    \            /'  //      ||
+        ,( ( ((, ))              ~-__>--<_~-_  ~--__---~'/'/  /'       VV
+      `~/  )` ) ,/|                 ~-_~>--<_/-__      __-~ _/
+    ._-~//( )/ )) `                    ~~-'_/_/ /~~~~~__--~
+     ;'( ')/ ,)(                              ~~~~~~~~
+    ' ') '( (/
+ME RIGHT NOW.
+FUCK YEAH
+ */
